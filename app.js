64e12920c1a4d732582b77620c146b980ffc833a
kawsar7790 <kawsaralam112307@gmail.com>
2024-02-05 16:38:05 +0600

    JS Code Fixer

--- app.js
+++ app.js
@@ -1,58 +1,43 @@
 
-
-const rightAnswers = ['B','B','B','B'];
+const rightAnswers = ['B', 'B', 'B', 'B'];
 const form = document.querySelector('.quiz-form');
 const result = document.querySelector('.result');
 
 
-form.addEventListener( 'submit' , e =>
-{
+form.addEventListener('submit', e => {
     e.preventDefault();
 
-
-
-
-    let score=0;
-
-    const userAnswers= [form.q1.value , form.q2.value , form.q3.value , form.q4.value ];
-    
-
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
 
     // check answers
-    userAnswers.forEach(( answers , index ) =>
-    {
-      
-        if (answers === rightAnswers[index])
-        {
+    userAnswers.forEach((answers, index) => {
+
+        if (answers === rightAnswers[index]) {
             score += 25;
         }
-        
 
-    } );
+    });
 
-    
     result.classList.remove('d-none'); //
 
 
+    scrollTo(0, 0)
+    console.log(document.querySelector('form'));
 
-scrollTo(0,0)
-console.log(document.querySelector('form'));
+    // marking system and animation
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
 
-// marking system and animation
-let output = 0;
-const timer = setInterval(() =>
-{
-    result.querySelector('span').textContent = `${output}%`;
-
-    if (output === score ) // if reaches to value stops
-    {
-     clearInterval(timer);   
-    }
-    else
-    {
-        output++;
-    }
-} ,10);
+        if (output === score) // if reaches to value stops
+        {
+            clearInterval(timer);
+        }
+        else {
+            output++;
+        }
+    }, 10);
 
 
 
